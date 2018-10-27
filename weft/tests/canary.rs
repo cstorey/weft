8c364514e15dccd77cc94257f4bc0c84eb535a0e
Ceri Storey <cez@necrofish.org.uk>
2018-10-27 14:28:34 +0100

    Move render api bits to seperate test file.

--- weft/tests/canary.rs
+++ weft/tests/canary.rs
@@ -1,32 +1,9 @@
 extern crate weft;
-
-use std::io;
-use weft::*;
 #[macro_use]
 extern crate weft_derive;
 
-#[test]
-fn should_render_trivial_example() {
-    struct TrivialExample;
-    // This simulates what a template of the form `<p>Hello</p>` should compile to.
-    impl Renderable for TrivialExample {
-        fn render_to<T: RenderTarget>(&self, mut target: T) -> Result<(), io::Error> {
-            target.start_element("p".into())?;
-            target.text("Hello".into())?;
-            target.end_element("p".into())?;
-            Ok(())
-        }
-    }
-
-    let s = render_to_string(TrivialExample).expect("render_to_string");
-    let expected = "<p>Hello</p>";
-    assert!(
-        s.contains(expected),
-        "String {:?} contains {:?}",
-        s,
-        expected
-    );
-}
+use std::io;
+use weft::*;
 
 #[derive(WeftTemplate)]
 #[template(path = "tests/trivial.html")]

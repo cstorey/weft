5d76b89014aaac7b377235928ba1edfb30196584
Ceri Storey <cez@necrofish.org.uk>
2018-10-29 20:27:42 +0000

    Hopefully minimise type shadowing in impl block.

--- derive/src/derive_renderable.rs
+++ derive/src/derive_renderable.rs
@@ -32,7 +32,7 @@ pub fn derive_impl(nodes: &[Handle], item: &syn::DeriveInput) -> Result<TokenStr
 
     let res = quote! {
         impl #impl_generics ::weft::Renderable for #ident #ty_generics #where_clause {
-            fn render_to<T: ::weft::RenderTarget>(&self, target: &mut T) -> Result<(), ::std::io::Error> {
+            fn render_to<__weft_R: ::weft::RenderTarget>(&self, target: &mut __weft_R) -> Result<(), ::std::io::Error> {
                 #impl_body;
                 Ok(())
             }

8ed975b6b99ca3ea8ac09f3812d922ec4fc2d7c8
Ceri Storey <cez@necrofish.org.uk>
2019-08-17 11:43:53 +0100

    Also ignore gh-pages on circleci.

--- .circleci/push-to-gh-pages.sh
+++ .circleci/push-to-gh-pages.sh
@@ -6,6 +6,7 @@ dir=$(mktemp -d /tmp/gh-pages-XXXXXXXXXXXXXXXXXXXXXXX)
 target_branch=gh-pages
 source="$1"
 version=${CIRCLE_SHA1:-$(git rev-parse HEAD)}
+cci_dir=${0%/*}
 
 upstream=${CIRCLE_REPOSITORY_URL:-$(git remote  get-url origin)}
 
@@ -21,7 +22,12 @@ if git -C "$dir" ls-files | grep -q .; then
 fi
 
 tar -C "$source" -c . | tar -C "$dir" -xv
+
+mkdir -p "${dir}/.circleci"
+cp "${cci_dir}/ignore-gh-pages.yml" "${dir}/.circleci/config.yml"
+
 git -C "$dir" add .
+
 changes=$(git -C "$dir" diff --cached --shortstat | tee /dev/stderr)
 if [[ -n "$changes" ]]; then
     git -C "$dir" commit -m "Commit docs from $upstream at $version"

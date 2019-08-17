4b33779617130690b68b869f2f794ad3b49afc78
Ceri Storey <cez@necrofish.org.uk>
2019-08-17 09:49:40 +0100

    Install mdbook.

---
+++ .circleci/mdbook-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+url=https://github.com/rust-lang-nursery/mdBook/releases/download/v0.3.1/mdbook-v0.3.1-x86_64-unknown-linux-gnu.tar.gz
+sha256sum=4511fb1d4d95331099a4c1777d6af8022ac5783af70b83f018c78c896a4027ab
+archive="mdbook.tar.gz"
+
+if [[ "$(uname -ms)" != "Linux x86_64" ]]; then
+    echo 1>&2 "Currently only works on Linux x86_64";
+    exit 1
+fi
+
+dir=$(mktemp -d /tmp/XXXXXXXX)
+archive_path="${dir}/${archive}"
+
+curl -Lo "${archive_path}" "$url"
+echo  "$sha256sum ${archive_path}" | sha256sum -c
+
+tar -zxvf "${archive_path}" mdbook
+./mdbook -V

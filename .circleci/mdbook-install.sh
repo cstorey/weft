e231efe6cef89cd01944acf812a74dc01d283358
Ceri Storey <cez@necrofish.org.uk>
2019-08-17 10:55:54 +0100

    Install mdbook into $PATH too.

--- .circleci/mdbook-install.sh
+++ .circleci/mdbook-install.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+target=${1}
+
 url=https://github.com/rust-lang-nursery/mdBook/releases/download/v0.3.1/mdbook-v0.3.1-x86_64-unknown-linux-gnu.tar.gz
 sha256sum=4511fb1d4d95331099a4c1777d6af8022ac5783af70b83f018c78c896a4027ab
 archive="mdbook.tar.gz"
@@ -17,5 +19,7 @@ archive_path="${dir}/${archive}"
 curl -Lo "${archive_path}" "$url"
 echo  "$sha256sum ${archive_path}" | sha256sum -c
 
-tar -zxvf "${archive_path}" mdbook
-./mdbook -V
+tar -zxvf "${archive_path}" -C "$dir" mdbook
+mkdir -vp "${target}"
+mv -v "${dir}/mdbook" "${target}/mdbook"
+"${target}/mdbook" -V

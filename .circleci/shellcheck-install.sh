38e2bba5fdd5782594a9a9fc823986555b421825
Ceri Storey <cez@necrofish.org.uk>
2019-08-17 10:47:39 +0100

    Install shellcheck insto $PATH.

--- .circleci/shellcheck-install.sh
+++ .circleci/shellcheck-install.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+target=${1}
+
 url=https://shellcheck.storage.googleapis.com/shellcheck-v0.7.0.linux.x86_64.tar.xz
 sha512sum=84e06bee3c8b8c25f46906350fb32708f4b661636c04e55bd19cdd1071265112d84906055372149678d37f09a1667019488c62a0561b81fe6a6b45ad4fae4ac0
 archive="shellcheck.tar.xz"
@@ -17,6 +19,7 @@ archive_path="${dir}/${archive}"
 curl -Lo "${archive_path}" "$url"
 echo  "$sha512sum ${archive_path}" | sha512sum -c
 
-tar -Jxvf "${archive_path}" shellcheck-v0.7.0/shellcheck
-mv -v shellcheck-v0.7.0/shellcheck ./
-./shellcheck --version
+tar -Jxvf "${archive_path}" -C "$dir" shellcheck-v0.7.0/shellcheck
+mkdir -vp "${target}"
+mv -v "${dir}/shellcheck-v0.7.0/shellcheck" "${target}/shellcheck"
+"${target}/shellcheck" --version
--- .circleci/shellcheck-run.sh
+++ .circleci/shellcheck-run.sh
@@ -6,4 +6,4 @@ git ls-files | \
     xargs file --mime-type | \
     grep -F text/x-shellscript | \
     cut -d : -f 1 | \
-    xargs ./shellcheck
+    xargs shellcheck

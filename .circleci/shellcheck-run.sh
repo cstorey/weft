05d948b93603f600ca405342f0472f787a047714
Ceri Storey <cez@necrofish.org.uk>
2019-08-17 10:25:46 +0100

    Use git/file to locate shell scripts.

--- .circleci/shellcheck-run.sh
+++ .circleci/shellcheck-run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-find .circleci -name '*.sh' -exec ./shellcheck {} \+
+set -euxo pipefail
+
+git ls-files | \
+    xargs file --mime-type | \
+    grep -F text/x-shellscript | \
+    cut -d : -f 1 | \
+    xargs ./shellcheck

d3f4053fb10ea5aa624b33b1450f397dcde6d53c
Ceri Storey <cez@necrofish.org.uk>
2020-04-08 09:38:46 +0100

    Add big table benchmark.

--- weft/benches/templates.rs
+++ weft/benches/templates.rs
@@ -20,6 +20,12 @@ struct Team {
     score: u8,
 }
 
+#[derive(weft::WeftRenderable)]
+#[template(path = "benches/big-table.html")]
+struct BigTable {
+    table: Vec<Vec<usize>>,
+}
+
 #[bench]
 pub fn teams(b: &mut test::Bencher) {
     let teams = Teams {
@@ -45,6 +51,41 @@ pub fn teams(b: &mut test::Bencher) {
     };
 
     let thunk = || weft::render_to_string(&teams);
+    b.bytes = thunk().expect("render").len().try_into().unwrap();
+
+    b.iter(thunk);
+}
+
+macro_rules! big_table_sized {
+    ($name: ident, $size: expr) => {
+        #[bench]
+        pub fn $name(b: &mut test::Bencher) {
+            big_table(b, $size);
+        }
+    };
+}
+big_table_sized!(big_table_001x001, 1);
+big_table_sized!(big_table_002x002, 2);
+big_table_sized!(big_table_004x004, 4);
+big_table_sized!(big_table_008x008, 8);
+big_table_sized!(big_table_016x016, 16);
+big_table_sized!(big_table_032x032, 32);
+big_table_sized!(big_table_064x064, 64);
+big_table_sized!(big_table_128x128, 128);
+big_table_sized!(big_table_256x256, 256);
+
+fn big_table(b: &mut test::Bencher, size: usize) {
+    let mut table = Vec::with_capacity(size);
+    for _ in 0..size {
+        let mut inner = Vec::with_capacity(size);
+        for i in 0..size {
+            inner.push(i);
+        }
+        table.push(inner);
+    }
+    let tmpl = BigTable { table };
+
+    let thunk = || weft::render_to_string(&tmpl);
     b.bytes = thunk().expect("render").len().try_into().unwrap();
 
     b.iter(thunk);
